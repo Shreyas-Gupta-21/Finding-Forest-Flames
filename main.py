10aeabe69cd6010e7a5bece960eb8f210566ad36
Shreyas-Gupta-21 <62789950+Shreyas-Gupta-21@users.noreply.github.com>
2021-05-31 23:16:49 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+def get_file():
+    # Read image file path from user, or use the default file
+    filename = input('Enter image file (or press enter for default): ')
+    if filename == '':
+        filename = DEFAULT_FILE
+    return filename
+
+
+if __name__ == '__main__':
+    main()
