7253ba6ef16919755bc031fa58a978a8a174474e
Shreyas-Gupta-21 <62789950+Shreyas-Gupta-21@users.noreply.github.com>
2021-05-31 23:18:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
+from simpleimage import SimpleImage
+
+INTENSITY_THRESHOLD = 1.0
+DEFAULT_FILE = 'images/greenland-fire.png'
+
+def find_flames(filename):
+    """
+    This function should highlight the "sufficiently red" pixels
+    in the image and grayscale all other pixels in the image
+    in order to highlight areas of wildfires.
+    """
+    image = SimpleImage(filename)
+    # TODO: your code here
+    for pixel in image:
+        avg= (pixel.red + pixel.green + pixel.blue)//3
+        if pixel.red >= avg * INTENSITY_THRESHOLD:
+            pixel.red =255
+            pixel.green=0
+            pixel.blue=0
+        else:
+            pixel.red = avg
+            pixel.green = avg
+            pixel.blue = avg
+    return image
+
 def main():
     # Get file and load image
     filename = get_file()
