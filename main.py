0fa9f95d539fb10e80c71745927dc3c9d3680377
Shreyas-Gupta-21 <62789950+Shreyas-Gupta-21@users.noreply.github.com>
2021-05-31 23:17:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+def main():
+    # Get file and load image
+    filename = get_file()
+    image = SimpleImage(filename)
+
+    # Show the original fire
+    original_fire = SimpleImage(filename)
+    original_fire.show()
+
+    # Show the highlighted fire
+    highlighted_fire = find_flames(filename)
+    highlighted_fire.show()
+
 def get_file():
     # Read image file path from user, or use the default file
     filename = input('Enter image file (or press enter for default): ')
